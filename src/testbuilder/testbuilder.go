2fa9f5019465422e9b5970ce436078b08665a8cb
mayleaf <uh.jeong651@gmail.com>
2019-11-25 09:03:22 +0000

    ADD TestBuilder

---
+++ src/testbuilder/testbuilder.go
@@ -0,0 +1,16 @@
+package testbuilder
+
+import (
+)
+
+
+type testbuilder struct {
+	Args []string
+}
+
+func (e *testbuilder) Build(args []string) {
+	//TODO checkArgument,parseArgument,clientsetting, if none is parameter print usage
+}
+func parseArgument
+func settingClient
+func printUsage
