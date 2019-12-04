e5ee0df3a44a68dc55d2aa51171063e4533ae598
mayleaf <uh.jeong651@gmail.com>
2019-12-04 07:49:05 +0000

    Separate request.json

--- src/main/main.go
+++ src/main/main.go
@@ -13,20 +13,34 @@ import (
 
 func main() {
 	//TODO Separate Start Process
+	first := flag.String("start", "start.json", "<file_name>.json")
 	request := flag.String("request", "request.json", "<file_name>.json")
+	finish := flag.String("finish", "finish.json", "<file_name>.json")
 	value := flag.String("value", "value.csv", "<file_name>.csv")
 	flag.Parse()
 
 	documents := readJson(*request)
+	starts := readJson(*first)
+	lasts := readJson(*finish)
 	rows := readCsv(*value)
+
 	var e = client.Client{}
 	e.SetConnection()
 	e.CreateThreads(rows)
 
+	for _, start := range starts {
+		message, _ := json.Marshal(start)
+		e.MakeRequest(string(message))
+	}
 	for _, document := range documents {
 		message, _ := json.Marshal(document)
 		e.MakeRequest(string(message))
 	}
+	for _, last := range lasts {
+		message, _ := json.Marshal(last)
+		e.MakeRequest(string(message))
+	}
+
 }
 
 func readJson(file_name string) map[string]interface{} {
