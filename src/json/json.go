daf7f80ded6410bc9ec50e15b8e36c3abcdda8a4
mayleaf <uh.jeong651@gmail.com>
2019-12-10 06:15:00 +0000

    Add Create Json

--- src/json/json.go
+++ src/json/json.go
@@ -3,10 +3,17 @@ Package Json implement a simple library for json CRUD.
 */
 package json
 
-import ()
+import (
+	"encoding/json"
+)
 
 type Json struct {
 	json map[string]interface{}
+}
+
+func (e *Json) Create(msg string) error {
+	err := json.Unmarshal([]byte(msg), e.json)
+	return err
 }
 
 //TODO update,create, delete, read
