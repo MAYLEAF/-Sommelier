93975e43b491e266c067077ef151d50e5c900741
mayleaf <uh.jeong651@gmail.com>
2019-12-10 05:37:09 +0000

    Add package json

---
+++ src/json/json.go
@@ -0,0 +1,12 @@
+/*
+Package Json implement a simple library for json CRUD.
+*/
+package json
+
+import ()
+
+type Json struct {
+	json map[string]interface{}
+}
+
+//TODO update,create, delete, read
