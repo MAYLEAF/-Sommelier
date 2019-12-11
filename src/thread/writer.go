01817859d2743e2bb251cdfcf6aa07c381f1809e
mayleaf <uh.jeong651@gmail.com>
2019-12-11 03:26:06 +0000

    Replace encoding/json to json package

--- src/thread/writer.go
+++ src/thread/writer.go
@@ -1,7 +1,7 @@
 package thread
 
 import (
-	"encoding/json"
+	"json"
 	"log"
 	"sync"
 )
@@ -10,19 +10,20 @@ type writer struct {
 	lock sync.Mutex
 }
 
-func (e *writer) write(thread *Handler, Message string) error {
+func (e *writer) write(thread *Handler, message string) error {
 	e.lock.Lock()
 	defer e.lock.Unlock()
 
-	var result interface{}
-	json.Unmarshal([]byte(Message), &result)
-	result.(interface{}).(map[string]interface{})["uid"] = thread.value[0]
-	message, _ := json.Marshal(result)
-	if _, err := thread.conn.Write([]byte(message)); nil != err {
+	msg := json.Json{}
+	msg.Create(message)
+	msg.Update("uid", thread.value[0])
+	content, _ := msg.Read()
+
+	if _, err := thread.conn.Write(content); nil != err {
 		log.Printf("failed to write err: %v", err)
 		return err
 	}
-	log.Print("Request Message:" + string(message))
+	log.Print("Request Message:" + string(content))
 
 	return nil
 }
