6e2129b772ea14d56a25484bb3b85a24299264b9
mayleaf <uh.jeong651@gmail.com>
2020-01-09 14:45:30 +0000

    Use encoding/json

--- thread/reader.go
+++ thread/reader.go
@@ -2,14 +2,18 @@ package thread
 
 import (
 	"bufio"
-	"github.com/MAYLEAF/Sommelier/json"
+	json "encoding/json"
+	jsonLib "github.com/MAYLEAF/Sommelier/json"
 	"github.com/MAYLEAF/Sommelier/logger"
 	"sync"
 )
 
 type reader struct {
-	thread Handler
-	lock   sync.Mutex
+	thread      Handler
+	jsonDecoder *json.Decoder
+	bufReader   *bufio.Reader
+	lock        sync.Mutex
+	once        sync.Once
 }
 
 func (e *reader) read(thread *Handler) []byte {
@@ -17,11 +21,15 @@ func (e *reader) read(thread *Handler) []byte {
 	defer e.lock.Unlock()
 
 	msg := make(map[string]interface{})
-	bufReader := bufio.NewReader(thread.conn)
-	if err := json.Decode(bufReader, msg); err != nil {
+	e.once.Do(func() {
+		e.bufReader = bufio.NewReader(thread.conn)
+		e.jsonDecoder = json.NewDecoder(e.bufReader)
+	})
+
+	if err := e.jsonDecoder.Decode(&msg); err != nil {
 		logger.Info("Thread Reader error occur Err: %v User:%v", err, thread.value[0])
 	}
-	buf := json.Read(msg)
+	buf := jsonLib.Read(msg)
 
 	logger.Info("Message from server - User=" + thread.value[0] + " " + string(buf) + "\n")
 	return buf
