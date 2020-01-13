7a435dbe0ae8dfb78ec58bfdd1da5b7904f60204
mayleaf <uh.jeong651@gmail.com>
2020-01-13 15:17:40 +0000

    Update context

--- thread/context.go
+++ thread/context.go
@@ -8,14 +8,10 @@ import (
 )
 
 type context struct {
-	msg              json.Json
-	ping_count       int
-	turn_seconds     int
-	is_finish_throw  bool
-	is_request_fired bool
-	is_host          bool
-	is_my_turn       bool
-	thread           *Handler
+	msg             json.Json
+	ping_count      int
+	is_finish_throw bool
+	is_host         bool
 }
 
 var Actions map[string]interface{}
@@ -24,25 +20,21 @@ var Usercount int
 func (e *context) Initialize(thread *Handler) {
 	e.msg = json.Json{}
 	e.msg.SetJson(Actions)
-	e.ping_count = 5
-	e.turn_seconds = 15
+	e.ping_count = 100
 	e.is_finish_throw = false
-	e.is_request_fired = false
 	e.is_host = false
-	e.is_my_turn = false
-	e.thread = thread
 }
 
-func (e *context) react(thread *Handler) {
+func (e *context) react(user *Handler) {
 	threadwriter := writer{}
 	threadreader := reader{}
 
 	chjson := make(chan []byte)
-	threadwriter.write(thread, json.Read(e.msg.Load("C_LOGIN_REQ")))
+	threadwriter.write(user, json.Read(e.msg.Load("C_LOGIN_REQ")))
 
 	go func() {
 		for {
-			response := threadreader.read(thread)
+			response := threadreader.read(user)
 			res := make(map[string]interface{})
 			jsonObj := json.Json{}
 			bytesReader := bytes.NewReader(response)
@@ -65,21 +57,21 @@ func (e *context) react(thread *Handler) {
 			}
 
 			if jsonObj.Load("_pcode") == "S_GAME_START" {
-				if jsonObj.Load("hostUid") == thread.value[0] {
+				if jsonObj.Load("hostUid") == user.value[0] {
 					e.is_host = true
 					chjson <- json.Read(e.msg.Load("C_GAME_DATA"))
 				}
 				continue
 			}
 			if jsonObj.Load("_pcode") == "C_GAME_DATA" && !e.is_finish_throw {
-				if jsonObj.Load("uid") == thread.value[0] {
+				if jsonObj.Load("uid") == user.id {
 					e.ping_count--
 				}
-				if jsonObj.Load("uid") == thread.value[0] && e.ping_count <= 0 {
+				if jsonObj.Load("uid") == user.id && e.ping_count <= 0 {
 					chjson <- json.Read(e.msg.Load("C_FINISH_GAME"))
 					e.is_finish_throw = true
 				}
-				if jsonObj.Load("uid") == thread.value[0] {
+				if jsonObj.Load("uid") == user.id {
 					continue
 				}
 				chjson <- json.Read(e.msg.Load("C_GAME_DATA"))
@@ -99,7 +91,7 @@ func (e *context) react(thread *Handler) {
 
 			if jsonObj.Load("_pcode") == "S_MATCHING_FAIL" {
 				chjson <- json.Read(e.msg.Load("C_BACK_TO_LOBBY"))
-				thread.Schedule.Done()
+				user.Schedule.Done()
 				return
 			}
 		}
@@ -109,35 +101,15 @@ func (e *context) react(thread *Handler) {
 		for {
 			select {
 			case msg := <-chjson:
-				if err := threadwriter.write(thread, msg); err != nil {
-					Usercount--
-					logger.Info("User count %d", Usercount)
-					thread.Schedule.Done()
+				if err := threadwriter.write(user, msg); err != nil {
+					user.Schedule.Done()
 					return
 				}
 				if reflect.DeepEqual(msg, json.Read(e.msg.Load("C_BACK_TO_LOBBY"))) {
-					Usercount--
-					logger.Info("User count %d", Usercount)
-					thread.Schedule.Done()
+					user.Schedule.Done()
 				}
 			}
 		}
 	}()
-
-	thread.Schedule.Wait()
-}
-
-func (e *context) TurnChange() {
-	e.is_my_turn = !e.is_my_turn
-	e.turn_seconds = 15
-	e.ping_count--
-}
-
-func (e *context) Send(pcode string) error {
-	writer := writer{}
-	if err := writer.write(e.thread, json.Read(e.msg.Load(pcode))); err != nil {
-		logger.Info("Thread context send fail User - %v, pcode: %v", e.thread.value[0], pcode)
-		return err
-	}
-	return nil
+	user.Schedule.Wait()
 }
